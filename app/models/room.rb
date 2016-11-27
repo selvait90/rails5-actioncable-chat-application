7bdc814628ea875bee454e6209c12bcfa3ef041d
Selvakumar Arumugam <selvait90@gmail.com>
2016-11-27 17:19:36 +0000

    Updated models

--- app/models/room.rb
+++ app/models/room.rb
@@ -1,3 +1,20 @@
 class Room < ApplicationRecord
   belongs_to :user
+  has_many :messages, dependent: :destroy
+  has_many :users, through: :messages
+  before_validation :sanitize, :namify
+
+
+  def to_param
+    self.name
+  end
+
+  def namify
+    self.name = self.title.downcase.gsub(" ", "-")
+  end
+
+  def sanitize
+    self.title = self.title.strip
+  end
+
 end
--- app/models/user.rb
+++ app/models/user.rb
@@ -1,2 +1,7 @@
 class User < ApplicationRecord
+  has_many :messages
+  has_many :rooms, through: :messages
+
+  validates :name, presence: true, uniqueness: true
+
 end
