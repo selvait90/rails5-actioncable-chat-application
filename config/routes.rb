ce8f4bdaf97a4ff78114651436ad24520cb7cdce
Selvakumar Arumugam <selvait90@gmail.com>
2016-11-27 17:33:43 +0000

    Added controllers

--- config/routes.rb
+++ config/routes.rb
@@ -1,3 +1,31 @@
 Rails.application.routes.draw do
+  get 'sessions/new'
+
+  get 'sessions/create'
+
+  get 'sessions/destroy'
+
+  get 'messages/create'
+
+  get 'rooms/new'
+
+  get 'rooms/create'
+
+  get 'rooms/update'
+
+  get 'rooms/edit'
+
+  get 'rooms/destroy'
+
+  get 'rooms/index'
+
+  get 'rooms/show'
+
+  get 'users/new'
+
+  get 'users/create'
+
+  get 'users/show'
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
